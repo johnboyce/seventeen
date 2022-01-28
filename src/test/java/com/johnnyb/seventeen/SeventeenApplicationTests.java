cddd3221a84c290ff2f8ba51a54adba84d85c46a
John Boyce <john_boyce@comcast.com>
2022-01-27 23:02:19 -0500

    Initial Commit Spring Boot and Java 7

---
+++ src/test/java/com/johnnyb/seventeen/SeventeenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnnyb.seventeen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeventeenApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
