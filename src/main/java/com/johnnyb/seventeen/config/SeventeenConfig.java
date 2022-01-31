dead711e4e40e61f6f129388e512bba761535cd7
John Boyce <john_boyce@comcast.com>
2022-01-30 23:12:46 -0500

    Get some basic stuff going

--- src/main/java/com/johnnyb/seventeen/config/SeventeenConfig.java
+++ src/main/java/com/johnnyb/seventeen/config/SeventeenConfig.java
@@ -1,2 +1,14 @@
-package com.johnnyb.seventeen.config;public class SeventeenConfig {
+package com.johnnyb.seventeen.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SeventeenConfig {
+
+  @Bean("defaultObjectMapper")
+  public ObjectMapper defaultObjectMapper(){
+    return new ObjectMapper();
+  }
 }
--- src/main/java/com/johnnyb/seventeen/controller/SeventeenRestController.java
+++ src/main/java/com/johnnyb/seventeen/controller/SeventeenRestController.java
@@ -1,2 +1,43 @@
-package com.johnnyb.seventeen.controller;public class SeventeenRestController {
+package com.johnnyb.seventeen.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class SeventeenRestController {
+
+  @Autowired
+  ObjectMapper defaultObjectMapper;
+
+  @Value("${spring.application.name}")
+  private String title;
+
+  @Value("${app.welcome.message}")
+  private String message;
+
+  private List<String> features = Arrays.asList("Pattern Matching for Switch (Preview)", "Remove RMI Activation", "Sealed Classes", "Deprecate the Applet API for Removal");
+
+  @GetMapping("/")
+  public String main(Model model) {
+    model.addAttribute("title", title);
+    model.addAttribute("message", message);
+    model.addAttribute("features", features);
+    return "welcome"; //view
+  }
+
+  @GetMapping("/name")
+  public JsonNode getSomeJson() {
+    ObjectNode objectNode = defaultObjectMapper.createObjectNode();
+    objectNode.put("yourName", "john");
+    return objectNode;
+  }
 }
