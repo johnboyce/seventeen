e735a62b8debf9808356dfeedc6074ac229df368
John Boyce <john_boyce@comcast.com>
2022-02-05 23:57:09 -0500

    - Add H2 Persistence - Add Rest API - Add Springdoc - Create and load sample data in h2

---
+++ src/main/java/com/johnnyb/seventeen/model/Information.java
@@ -0,0 +1,52 @@
+package com.johnnyb.seventeen.model;
+
+public class Information {
+  String firstName;
+  String lastName;
+  String email;
+  String details;
+  String description;
+
+  public Information() {
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getDetails() {
+    return details;
+  }
+
+  public void setDetails(String details) {
+    this.details = details;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
