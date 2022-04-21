28074ab4d04ce1708fcbadbeca3ccae4415172f8
John Boyce <john_boyce@comcast.com>
2022-04-20 21:31:14 -0400

    Added ECS to terraform - Added ECS service resource also - Added sg info for networking mode etc - configured taskdef in terraform also

---
+++ cicd/terraform/ecs_service.tf
@@ -0,0 +1,12 @@
+resource "aws_ecs_service" "seventeen" {
+  name = var.project_name # Should be the name of the service
+  launch_type = "FARGATE"
+  task_definition = "seventeen-task-definition"
+
+  network_configuration {
+    subnets = ["subnet-0c74a32f65cdd2b48", "subnet-077a613915e0e3b02"]
+    security_groups = ["sg-076e450d6b2cafad9"]
+    assign_public_ip = true
+  }
+
+}
